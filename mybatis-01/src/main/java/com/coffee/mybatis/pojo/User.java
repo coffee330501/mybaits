99c1e42d72e10df7e1f6ccfff11a8ddbf8e4514e
objcfeng <1034440274@qq.com>
2022-10-12 08:21:21 +0800

    10-12

---
+++ mybatis-01/src/main/java/com/coffee/mybatis/pojo/User.java
@@ -0,0 +1,11 @@
+package com.coffee.mybatis.pojo;
+
+@Data
+public class User {
+   private Integer id;
+   private String username;
+   private String password;
+   private Integer age;
+   private String sex;
+   private String email;
+}
