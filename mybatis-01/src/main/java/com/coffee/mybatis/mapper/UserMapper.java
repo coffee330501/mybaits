389401c2508cd3945588cc1cbeab4194d30d052b
objcfeng <1034440274@qq.com>
2022-10-16 21:48:30 +0800

    crud

--- mybatis-01/src/main/java/com/coffee/mybatis/mapper/UserMapper.java
+++ mybatis-01/src/main/java/com/coffee/mybatis/mapper/UserMapper.java
@@ -2,8 +2,18 @@ package com.coffee.mybatis.mapper;
 
 import com.coffee.mybatis.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface UserMapper {
     int insertUser();
+    void updateUser();
+    User getUserById(int id);
+    List<User> getAllUser();
+    User getUserByUsername(String username);
+    boolean checkLogin(@Param("username") String username,@Param("password") String password);
+    boolean checkLoginByMap(Map<String,Object> map);
 }
--- mybatis-01/src/test/java/com/coffee/mybatis/MybatisTest.java
+++ mybatis-01/src/test/java/com/coffee/mybatis/MybatisTest.java
@@ -2,6 +2,7 @@ package com.coffee.mybatis;
 
 
 import com.coffee.mybatis.mapper.UserMapper;
+import com.coffee.mybatis.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +11,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
 
 public class MybatisTest {
     @Test
@@ -22,6 +25,30 @@ public class MybatisTest {
             int i = userMapper.insertUser();
             System.out.println(i);
             session.commit();
+        }
+    }
+
+    @Test
+    public void testMybatis2() throws IOException {
+        InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = builder.build(stream);
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+//            userMapper.updateUser();
+//           User user=  userMapper.getUserById(3);
+//            System.out.println(user.getUsername());
+//            List<User> users = userMapper.getAllUser();
+//            users.forEach(System.out::println);
+//            User user = userMapper.getUserByUsername("张三");
+//            System.out.println(user);
+//            boolean checkLogin = userMapper.checkLogin("张三", "123456");
+//            System.out.println(checkLogin);
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("username","admin");
+            map.put("password","123456");
+            boolean checkLogin = userMapper.checkLoginByMap(map);
+            System.out.println(checkLogin);
         }
     }
 }
